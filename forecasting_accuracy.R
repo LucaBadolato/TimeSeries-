30d4413fbfdc7098e8cc7aa30450532cc351409d
Simone <simone.arrigoni.95@gmail.com>
2020-05-29 18:38:41 +0200

    Structural model

--- forecasting_accuracy.R
+++ forecasting_accuracy.R
@@ -4,6 +4,10 @@ MSE_LG <- round(mean((LG_Filt$f-y)^2),3) # mean square error (MSE)
 MAE_LG <- round(mean(abs(LG_Filt$f-y)),3) # mean abs. error (MAE)
 MAPE_LG <- round(mean(abs(LG_Filt$f-y)/y),3) # mean abs. percentage error (MAPE)
 
+MSE_BSM <- round(mean(BSM$residuals),3) # mean square error (MSE)
+MAE_BSM <- round(mean(abs(BSM$residuals)),3) # mean abs. error (MAE)
+MAPE_BSM <- round(mean(abs(BSM$residuals)/y),3) # mean abs. percentage error (MAPE)
+
 MSE_DR <- round(mean((DR_Filt$f-y)^2),3) # mean square error (MSE)
 MAE_DR <- round(mean(abs(DR_Filt$f-y)),3) # mean abs. error (MAE)
 MAPE_DR <- round(mean(abs(DR_Filt$f-y)/y),3) # mean abs. percentage error (MAPE)
