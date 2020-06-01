c8c5b094cbcefdfeab4bc76912013197c3988b21
simovalle96 <simovalle@live.it>
2020-06-01 03:17:41 +0200

    .

--- forecasting_accuracy.R
+++ forecasting_accuracy.R
@@ -1,5 +1,9 @@
 # Measures of forecasting accuracy for all models
 
+# MSE_HMM
+# MSE_HMM
+# MSE_HMM
+
 MSE_LG <- round(mean((LG_Filt$f-y)^2),3) # mean square error (MSE)
 MAE_LG <- round(mean(abs(LG_Filt$f-y)),3) # mean abs. error (MAE)
 MAPE_LG <- round(mean(abs(LG_Filt$f-y)/y),3) # mean abs. percentage error (MAPE)
@@ -12,9 +16,13 @@ MSE_LG_3 <- round(mean((LG_Filt_3$f-y)^2),3) # mean square error (MSE)
 MAE_LG_3 <- round(mean(abs(LG_Filt_3$f-y)),3) # mean abs. error (MAE)
 MAPE_LG_3 <- round(mean(abs(LG_Filt_3$f-y)/y),3) # mean abs. percentage error (MAPE)
 
-MSE_BSM <- round(mean(BSM$residuals),3) # mean square error (MSE)
-MAE_BSM <- round(mean(abs(BSM$residuals)),3) # mean abs. error (MAE)
-MAPE_BSM <- round(mean(abs(BSM$residuals)/y),3) # mean abs. percentage error (MAPE)
+MSE_LG_v <- round(mean(LG_v_Filt$f-y),3) # mean square error (MSE)
+MAE_LG_v <- round(mean(abs(LG_v_Filt$f-y)),3) # mean abs. error (MAE)
+MAPE_LG_v <- round(mean(abs(LG_v_Filt$f-y)/y),3) # mean abs. percentage error (MAPE)
+
+MSE_LG_v_seas <- round(mean(LG_v_seas_Filt$f-y),3) # mean square error (MSE)
+MAE_LG_v_seas <- round(mean(abs(LG_v_seas_Filt$f-y)),3) # mean abs. error (MAE)
+MAPE_LG_v_seas <- round(mean(abs(LG_v_seas_Filt$f-y)/y),3) # mean abs. percentage error (MAPE)
 
 MSE_DR <- round(mean((DR_Filt$f-y)^2),3) # mean square error (MSE)
 MAE_DR <- round(mean(abs(DR_Filt$f-y)),3) # mean abs. error (MAE)
