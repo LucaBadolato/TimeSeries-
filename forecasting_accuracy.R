867bbdd624fef96a063c89a42942d353c2d25c53
Simone <simone.arrigoni.95@gmail.com>
2020-06-03 20:25:50 +0200

    SUTSE

--- forecasting_accuracy.R
+++ forecasting_accuracy.R
@@ -28,10 +28,6 @@ MSE_DR <- round(mean((DR_Filt$f-y)^2),3) # mean square error (MSE)
 MAE_DR <- round(mean(abs(DR_Filt$f-y)),3) # mean abs. error (MAE)
 MAPE_DR <- round(mean(abs(DR_Filt$f-y)/y),3) # mean abs. percentage error (MAPE)
 
-MSE_SUR <- round(mean((SUR_Filt$f-y)^2),3) # mean square error (MSE)
-MAE_SUR <- round(mean(abs(SUR_Filt$f-y)),3) # mean abs. error (MAE)
-MAPE_SUR <- round(mean(abs(SUR_Filt$f-y)/y),3) # mean abs. percentage error (MAPE)
-
 MSE_SUTSE <- round(mean((SUTSE_Filt$f-y)^2),3) # mean square error (MSE)
 MAE_SUTSE <- round(mean(abs(SUTSE_Filt$f-y)),3) # mean abs. error (MAE)
 MAPE_SUTSE <- round(mean(abs(SUTSE_Filt$f-y)/y),3) # mean abs. percentage error (MAPE)
