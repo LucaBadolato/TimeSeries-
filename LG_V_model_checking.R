dc1861976c81630df35a19d2e7234c4f8b32391c
Simone <simone.arrigoni.95@gmail.com>
2020-06-04 17:45:04 +0200

    Mod check

--- LG_V_model_checking.R
+++ LG_V_model_checking.R
@@ -1,25 +1,31 @@
-knitr::opts_chunk$set(fig.width=6, fig.height=3,
+knitr::opts_chunk$set(fig.width=8, fig.height=4, 
                       fig.cap="\\label{fig:diagnostic}
                       (a) Standardized one-step-ahead forecast errors;
                       (b) ACF of one-step-ahead forecast errors;
                       (c) Normal probability plot of standardized one-step-ahead forecast errors")
 
-# Dynamic linear regression: Model checking
+# Linear growth model: Model checking
 
-res_DR <- residuals(DR_Filt, sd=FALSE)
-res_DR = data.frame(res_DR, louisiana_df$n_months)
-colnames(res_DR) = c("res", "time")
+res_LG_v <- residuals(LG_v_Filt, sd=FALSE)
+res_LG_v = data.frame(res_LG_v,louisiana_df$n_months)
+colnames(res_LG_v) = c("res", "time")
 
-a <- ggplot(res_DR, aes(x=time)) +
-  geom_line(aes(y=res), size=0.4) +
-  theme_classic(base_size=9.5) +
-  theme(panel.grid.minor = element_line(size=0.5)) +
+ggplot(res_LG_v, aes(x=time)) +
+  geom_line(aes(y=res), size=0.5) +
+  theme_bw(base_size=9) +
   labs(x="Year", y="Residuals") +
-  scale_x_continuous(breaks=seq(0,200,25),labels=format(seq(as.Date("2001/1/1"),as.Date("2017/6/1"), "2 years"), "%Y")) +
-  scale_y_continuous(breaks=seq(-4.5,4.5,1), limits = c(-3,4.5)) +
-  theme(axis.text=element_text(size=12),
-        axis.title.x=element_text(size=12, family="Times", margin=margin(t=5)),
-        axis.title.y=element_text(size=12, family="Times", margin=margin(r=5)))
+  scale_x_continuous(breaks=seq(0,186,24),
+                     labels=format(seq(as.Date("2001/1/1"),as.Date("2016/6/1"), "2 years"), "%Y")) +
+  scale_y_continuous(breaks=seq(-5,5,1), limits = c(-3,4)) +
+  theme(axis.text.x=element_text(size=10, family="Times", margin=margin(t=5)),
+        axis.text.y=element_text(size=10, family="Times", margin=margin(r=5)),
+        axis.title.x=element_text(size=10, family="Times", margin=margin(t=5)),
+        axis.title.y=element_text(size=10, family="Times", margin=margin(r=5)),
+        panel.grid.major=element_line(size=0.5),
+        panel.grid.minor=element_blank(),
+        plot.margin=unit(c(0,0,0,0.8), "cm"))
+
+
 
 ggplot.corr <- function(data, lag.max = 24, ci = 0.95, large.sample.size = TRUE, horizontal = TRUE,...)
 {
@@ -53,9 +59,9 @@ ggplot.corr <- function(data, lag.max = 24, ci = 0.95, large.sample.size = TRUE,
       scale_x_continuous(breaks = seq(0,max(df1$lag),6)) +
       scale_y_continuous(name = element_blank(), 
                          limits = c(min(df1$acf,df2$pacf),1)) +
-      theme(axis.text=element_text(size=12),
-            axis.title.x=element_text(size=12,family="Times", margin=margin(t=5)),
-            axis.title.y=element_text(size=12,family="Times", margin=margin(r=5)),
+      theme(axis.text=element_text(size=10),
+            axis.title.x=element_text(size=10,family="Times", margin=margin(t=5)),
+            axis.title.y=element_text(size=10,family="Times", margin=margin(r=5)),
             panel.grid.minor = element_line(size=0.5))
     
     plot.pacf <- ggplot(data = df2, aes(x = lag, y = pacf)) +
@@ -67,9 +73,9 @@ ggplot.corr <- function(data, lag.max = 24, ci = 0.95, large.sample.size = TRUE,
       scale_y_continuous(name = element_blank(),
                          limits = c(min(df1$acf,df2$pacf),1)) +
       ggtitle("PACF") +
-      theme(axis.text=element_text(size=12),
-            axis.title.x=element_text(size=12,family="Times", margin=margin(t=5)),
-            axis.title.y=element_text(size=12,family="Times", margin=margin(r=5)),
+      theme(axis.text=element_text(size=10),
+            axis.title.x=element_text(size=10,family="Times", margin=margin(t=5)),
+            axis.title.y=element_text(size=10,family="Times", margin=margin(r=5)),
             panel.grid.minor = element_line(size=0.5))
   }
   else
@@ -86,9 +92,9 @@ ggplot.corr <- function(data, lag.max = 24, ci = 0.95, large.sample.size = TRUE,
       scale_x_continuous(breaks = seq(0,max(df1$lag),6)) +
       scale_y_continuous(name = element_blank(), 
                          limits = c(min(df1$acf,df2$pacf),1)) +
-      theme(axis.text=element_text(size=12),
-            axis.title.x=element_text(size=12,family="Times", margin=margin(t=5)),
-            axis.title.y=element_text(size=12,family="Times", margin=margin(r=5)),
+      theme(axis.text=element_text(size=10),
+            axis.title.x=element_text(size=10,family="Times", margin=margin(t=5)),
+            axis.title.y=element_text(size=10,family="Times", margin=margin(r=5)),
             panel.grid.minor = element_line(size=0.5))
     
     plot.pacf <- ggplot(data = df2, aes(x = lag, y = pacf)) +
@@ -104,24 +110,24 @@ ggplot.corr <- function(data, lag.max = 24, ci = 0.95, large.sample.size = TRUE,
       scale_y_continuous(name = element_blank(),
                          limits = c(min(df1$acf,df2$pacf),1)) +
       ggtitle("PACF") +
-      theme(axis.text=element_text(size=12),
-            axis.title.x=element_text(size=12,family="Times", margin=margin(t=5)),
-            axis.title.y=element_text(size=12,family="Times", margin=margin(r=5)),
+      theme(axis.text=element_text(size=10),
+            axis.title.x=element_text(size=10,family="Times", margin=margin(t=5)),
+            axis.title.y=element_text(size=10,family="Times", margin=margin(r=5)),
             panel.grid.minor=element_line(size=0.5))
   }
   cowplot::plot_grid(plot.acf)
 }
 
-b <- ggplot.corr(data=res_DR$res, lag.max=24, ci=0.95, large.sample.size=FALSE, horizontal=TRUE)
+b <- ggplot.corr(data=res_LG_v$res, lag.max=12, ci=0.95, large.sample.size=FALSE, horizontal=TRUE)
 
-c <- ggplot(res_DR, aes(sample=res)) +
-  stat_qq(col="blue") +
+c <- ggplot(res_LG_v, aes(sample=res)) +
+  stat_qq(col="blue",size=0.5) +
   stat_qq_line(col="red", lty=2, size=1) +
   scale_y_continuous(breaks=seq(-3, 3,1), limits=c(-3,3)) +
   scale_x_continuous(breaks=seq(-3, 3,1), limits=c(-3,3)) +
   theme_classic(base_size=9.5) +
   theme(panel.grid.minor=element_line(size=0.5)) +
   labs(x="Theoretical", y="Observed") +
-  theme(axis.text=element_text(size=12),
-        axis.title.x=element_text(size=12,family="Times", margin=margin(t=5)),
-        axis.title.y=element_text(size=12,family="Times", margin=margin(r=5)))
+  theme(axis.text=element_text(size=10),
+        axis.title.x=element_text(size=10,family="Times", margin=margin(t=5)),
+        axis.title.y=element_text(size=10,family="Times", margin=margin(r=5)))
