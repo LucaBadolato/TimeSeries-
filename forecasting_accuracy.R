33f78c93b26cf479d4edbfa38b1a8ae024b6a569
simovalle96 <simovalle@live.it>
2020-06-04 17:25:38 +0200

    .

--- forecasting_accuracy.R
+++ forecasting_accuracy.R
@@ -1,8 +1,8 @@
 # Measures of forecasting accuracy for all models
 
-# MSE_HMM
-# MSE_HMM
-# MSE_HMM
+MSE_HMM <- round(mean((HMM_forecast-y)^2),3) # mean square error (MSE)
+MAE_HMM <- round(mean(abs(HMM_forecast-y)),3) # mean abs. error (MAE)
+MAPE_HMM <- round(mean(abs(HMM_forecast-y)/y),3) # mean abs. percentage error (MAPE)
 
 MSE_LG <- round(mean((LG_Filt$f-y)^2),3) # mean square error (MSE)
 MAE_LG <- round(mean(abs(LG_Filt$f-y)),3) # mean abs. error (MAE)
@@ -28,6 +28,6 @@ MSE_DR <- round(mean((DR_Filt$f-y)^2),3) # mean square error (MSE)
 MAE_DR <- round(mean(abs(DR_Filt$f-y)),3) # mean abs. error (MAE)
 MAPE_DR <- round(mean(abs(DR_Filt$f-y)/y),3) # mean abs. percentage error (MAPE)
 
-# MSE_SUTSE <- round(mean((SUTSE_Filt$f-y)^2),3) # mean square error (MSE)
-# MAE_SUTSE <- round(mean(abs(SUTSE_Filt$f-y)),3) # mean abs. error (MAE)
-# MAPE_SUTSE <- round(mean(abs(SUTSE_Filt$f-y)/y),3) # mean abs. percentage error (MAPE)
+MSE_SUTSE <- round(mean((SUTSE_Filt$f[,1]-y)^2),3) # mean square error (MSE)
+MAE_SUTSE <- round(mean(abs(SUTSE_Filt$f[,1]-y)),3) # mean abs. error (MAE)
+MAPE_SUTSE <- round(mean(abs(SUTSE_Filt$f[,1]-y)/y),3) # mean abs. percentage error (MAPE)
