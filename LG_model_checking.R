9212c955c8f041cd8ceada059fbca90c47cc816b
simovalle96 <simovalle@live.it>
2020-06-05 02:23:23 +0200

    .

--- LG_model_checking.R
+++ LG_model_checking.R
@@ -18,13 +18,13 @@ a <- ggplot(res_LG, aes(x=time)) +
   scale_x_continuous(breaks=seq(0,186,24),
                      labels=format(seq(as.Date("2001/1/1"),as.Date("2016/6/1"), "2 years"), "%Y")) +
   scale_y_continuous(breaks=seq(-4.5,4.5,1), limits = c(-3,4.5)) +
-  theme(axis.text.x=element_text(size=20, family="Times", margin=margin(t=5)),
-        axis.text.y=element_text(size=20, family="Times", margin=margin(r=5)),
+  theme(axis.text.x=element_text(size=15, family="Times", margin=margin(t=5)),
+        axis.text.y=element_text(size=15, family="Times", margin=margin(r=5)),
         axis.title.x=element_blank(),
-        axis.title.y=element_text(size=20, family="Times", margin=margin(r=5)),
+        axis.title.y=element_text(size=15, family="Times", margin=margin(r=5)),
         panel.grid.major=element_line(size=0.5),
         panel.grid.minor=element_blank(),
-        plot.margin=unit(c(0,0,1,2), "cm"))
+        plot.margin=unit(c(0,0,1,1.8), "cm"))
 
 ggplot.corr <- function(data, lag.max = 24, ci = 0.95, large.sample.size = TRUE, horizontal = TRUE,...)
 {
@@ -58,13 +58,13 @@ ggplot.corr <- function(data, lag.max = 24, ci = 0.95, large.sample.size = TRUE,
       scale_x_continuous(breaks = seq(0,max(df1$lag),1)) +
       scale_y_continuous(name = element_blank(), 
                          limits = c(min(df1$acf,df2$pacf),1)) +
-      theme(axis.text.x=element_text(size=20, family="Times", margin=margin(t=5)),
-            axis.text.y=element_text(size=20, family="Times", margin=margin(r=5)),
+      theme(axis.text.x=element_text(size=15, family="Times", margin=margin(t=5)),
+            axis.text.y=element_text(size=15, family="Times", margin=margin(r=5)),
             axis.title.x=element_blank(),
-            axis.title.y=element_text(size=20, family="Times", margin=margin(r=5)),
+            axis.title.y=element_text(size=15, family="Times", margin=margin(r=5)),
             panel.grid.major=element_line(size=0.5),
             panel.grid.minor=element_blank(),
-            plot.margin=unit(c(0,0,1,2), "cm"))
+            plot.margin=unit(c(0,0,1,1.8), "cm"))
     
     plot.pacf <- ggplot(data = df2, aes(x = lag, y = pacf)) +
       geom_area(aes(x = lag, y = qnorm((1+ci)/2)*pacfstd), fill = "#B9CFE7") +
@@ -75,13 +75,13 @@ ggplot.corr <- function(data, lag.max = 24, ci = 0.95, large.sample.size = TRUE,
       scale_y_continuous(name = element_blank(),
                          limits = c(min(df1$acf,df2$pacf),1)) +
       ggtitle("PACF") +
-      theme(axis.text.x=element_text(size=20, family="Times", margin=margin(t=5)),
-            axis.text.y=element_text(size=20, family="Times", margin=margin(r=5)),
+      theme(axis.text.x=element_text(size=15, family="Times", margin=margin(t=5)),
+            axis.text.y=element_text(size=15, family="Times", margin=margin(r=5)),
             axis.title.x=element_blank(),
-            axis.title.y=element_text(size=20, family="Times", margin=margin(r=5)),
+            axis.title.y=element_text(size=15, family="Times", margin=margin(r=5)),
             panel.grid.major=element_line(size=0.5),
             panel.grid.minor=element_blank(),
-            plot.margin=unit(c(0,0,1,2), "cm"))
+            plot.margin=unit(c(0,0,1,1.8), "cm"))
   }
   else
   {
@@ -97,13 +97,13 @@ ggplot.corr <- function(data, lag.max = 24, ci = 0.95, large.sample.size = TRUE,
       scale_x_continuous(breaks = seq(0,max(df1$lag),1)) +
       scale_y_continuous(name = element_blank(), 
                          limits = c(min(df1$acf,df2$pacf),1)) +
-      theme(axis.text.x=element_text(size=20, family="Times", margin=margin(t=5)),
-            axis.text.y=element_text(size=20, family="Times", margin=margin(r=5)),
+      theme(axis.text.x=element_text(size=15, family="Times", margin=margin(t=5)),
+            axis.text.y=element_text(size=15, family="Times", margin=margin(r=5)),
             axis.title.x=element_blank(),
-            axis.title.y=element_text(size=20, family="Times", margin=margin(r=5)),
+            axis.title.y=element_text(size=15, family="Times", margin=margin(r=5)),
             panel.grid.major=element_line(size=0.5),
             panel.grid.minor=element_blank(),
-            plot.margin=unit(c(0,0,1,2), "cm"))
+            plot.margin=unit(c(0,0,1,1.8), "cm"))
     
     plot.pacf <- ggplot(data = df2, aes(x = lag, y = pacf)) +
       geom_col(fill = "#4373B6", width = 0.7) +
@@ -118,13 +118,13 @@ ggplot.corr <- function(data, lag.max = 24, ci = 0.95, large.sample.size = TRUE,
       scale_y_continuous(name = element_blank(),
                          limits = c(min(df1$acf,df2$pacf),1)) +
       ggtitle("PACF") +
-      theme(axis.text.x=element_text(size=20, family="Times", margin=margin(t=5)),
-            axis.text.y=element_text(size=20, family="Times", margin=margin(r=5)),
+      theme(axis.text.x=element_text(size=15, family="Times", margin=margin(t=5)),
+            axis.text.y=element_text(size=15, family="Times", margin=margin(r=5)),
             axis.title.x=element_blank(),
-            axis.title.y=element_text(size=20, family="Times", margin=margin(r=5)),
+            axis.title.y=element_text(size=15, family="Times", margin=margin(r=5)),
             panel.grid.major=element_line(size=0.5),
             panel.grid.minor=element_blank(),
-            plot.margin=unit(c(0,0,1,2), "cm"))
+            plot.margin=unit(c(0,0,1,1.8), "cm"))
   }
   cowplot::plot_grid(plot.acf)
 }
@@ -138,10 +138,10 @@ c <- ggplot(res_LG, aes(sample=res)) +
   scale_x_continuous(breaks=seq(-3, 3,1), limits=c(-3,3)) +
   theme_bw(base_size=9) +
   labs(x="Theoretical", y="Observed") +
-  theme(axis.text.x=element_text(size=20, family="Times", margin=margin(t=5)),
-        axis.text.y=element_text(size=20, family="Times", margin=margin(r=5)),
+  theme(axis.text.x=element_text(size=15, family="Times", margin=margin(t=5)),
+        axis.text.y=element_text(size=15, family="Times", margin=margin(r=5)),
         axis.title.x=element_blank(),
-        axis.title.y=element_text(size=20, family="Times", margin=margin(r=5)),
+        axis.title.y=element_text(size=15, family="Times", margin=margin(r=5)),
         panel.grid.major=element_line(size=0.5),
         panel.grid.minor=element_blank(),
         plot.margin=unit(c(0,0,1,1), "cm"))
